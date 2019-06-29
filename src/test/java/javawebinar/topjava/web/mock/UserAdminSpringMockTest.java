1aca33dc85a49a18c47750485a7473e1bdbe7698
Max <maksym.ivanov@leroymerlin.ua>
2019-06-29 22:38:33 +0300

    Implements UserAdminSpringMockTest Add spring-test and postgres dependencies

---
+++ src/test/java/javawebinar/topjava/web/mock/UserAdminSpringMockTest.java
@@ -0,0 +1,34 @@
+package javawebinar.topjava.web.mock;
+
+import javawebinar.topjava.model.Role;
+import javawebinar.topjava.model.User;
+import javawebinar.topjava.web.user.AdminUserRestController;
+import javawebinar.util.exception.NotFoundException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserAdminSpringMockTest {
+	@Autowired
+	AdminUserRestController controller;
+
+	@Test
+	public void testCreate() throws Exception {
+		controller.create(new User(null, "Name", "email", "password", true, Role.ROLE_USER));
+	}
+
+	@Test
+	public void testDelete() throws Exception {
+		controller.delete(7);
+	}
+
+	@Test(expected = NotFoundException.class)
+	public void testDeleteNotFound() throws Exception {
+		controller.delete(0);
+	}
+}
