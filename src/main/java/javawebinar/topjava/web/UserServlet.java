11da060fe5239a3ba411908e2a63846a06f675c6
Max <maksym.ivanov@leroymerlin.ua>
2019-07-21 21:18:06 +0300

    Add tomcat and spring-web dependencies

--- src/main/java/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/javawebinar/topjava/web/UserServlet.java
@@ -1,7 +1,11 @@
 package javawebinar.topjava.web;
 
 import javawebinar.topjava.LoggerWrapper;
+import javawebinar.topjava.service.UserService;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,13 +14,21 @@ import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
 	private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
+	private WebApplicationContext wac;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		wac = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		LOG.debug("redirect to userList");
-
-//		req.getRequestDispatcher("/userList.jsp").forward(req, resp);
-		resp.sendRedirect("userList.jsp");
+		UserService userService = wac.getBean(UserService.class);
+		req.setAttribute("userList", userService.getAll());
+		req.getRequestDispatcher("/userList.jsp").forward(req, resp);
+//		resp.sendRedirect("userList.jsp");
 	}
 
 	@Override
