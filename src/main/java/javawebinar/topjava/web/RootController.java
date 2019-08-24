e3430bfae4b1b700f0934de097b98bb634f736e4
Max <maksym.ivanov@leroymerlin.ua>
2019-08-24 13:46:26 +0300

    Controller refactoring

--- src/main/java/javawebinar/topjava/web/RootController.java
+++ src/main/java/javawebinar/topjava/web/RootController.java
@@ -1,4 +1,4 @@
-package javawebinar.topjava.web.user;
+package javawebinar.topjava.web;
 
 
 import org.springframework.stereotype.Controller;
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class UserController {
+public class RootController {
+	@RequestMapping(value = "/meals", method = RequestMethod.GET)
+	public String mealList() {
+		return "mealList";
+	}
+
 	@RequestMapping(value = "/users", method = RequestMethod.GET)
 	public String userList() {
 		return "userList";
