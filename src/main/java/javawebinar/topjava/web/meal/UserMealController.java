4c69115332da424a10bb9ed5d95ad1a47e5189a0
Max <maksym.ivanov@leroymerlin.ua>
2019-08-24 09:33:52 +0300

    Refactoring UserMealController

--- src/main/java/javawebinar/topjava/web/meal/UserMealController.java
+++ src/main/java/javawebinar/topjava/web/meal/UserMealController.java
@@ -1,21 +1,14 @@
 package javawebinar.topjava.web.meal;
 
-import javawebinar.topjava.LoggedUser;
-import javawebinar.topjava.service.UserMealService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserMealController {
-	@Autowired
-	private UserMealService service;
-
 	@RequestMapping(value = "/meals", method = RequestMethod.GET)
-	public String mealList(final Model model) {
-		model.addAttribute("mealList", service.getAll(LoggedUser.id()));
+	public String mealList() {
 		return "mealList";
 	}
 }
